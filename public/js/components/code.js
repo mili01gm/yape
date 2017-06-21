9d85691f2cabf83193b9265c8d18e2664d761afb
Luz Gutierrez <mili01gm@gmail.com>
2017-06-21 14:21:40 -0500

    jugando con el icono del input

--- public/js/components/code.js
+++ public/js/components/code.js
@@ -7,20 +7,20 @@ const Code = (data, update) => {
     const icon = $('<img src="img/icons/message.png"/>');
     const divText = $('<div class="page-text"></div>');
     const h3 = $('<h3>Ahora ingresa tu código/h3>');
-    const p = $('<p class="text-grey">Enviamos un SMS con el código de validación <br>al número <strong>${data.phone}</strong></p>');
+    const p = $('<p class="text-grey">`Enviamos un SMS con el código de validación <br>al número <strong>${data.phone}</strong>`</p>');
     divText.append(h3);
     divText.append(p);
     step.append(icon);
     step.append(divText);
 
     const form = $('<form id="send-code"></form>');
-    const input = $('<input type="tel" name="reg-phone" id="tel" class="form-input"/><i src="img/icons/lock.png">');
+    const input = $('<input type="tel" name="reg-phone" id="tel" class="form-input"/>');
     // const areaCode = $('<i src="img/icons/phoneandnumber.png">');
     const timer = $('');
 
     // input.append(areaCode);
     form.append(input);
-    form append(timer);
+    // form append(timer);
     section.append(step);
     section.append(form);
 
