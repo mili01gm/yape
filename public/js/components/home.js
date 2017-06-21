c321e342099cc91d590a63d419aeb3a7f1ba7bbf
Luz Gutierrez <mili01gm@gmail.com>
2017-06-21 11:22:11 -0500

    Las ramas no están jalando los avances de otras o master

--- public/js/components/home.js
+++ public/js/components/home.js
@@ -1,44 +1,20 @@
 'use strict';
 
 const Home = (update) => {
-    const section = $('<section class="container home"></section>');
-
+    /*const section = $('<section class="container"></section>');
     const divSlide = $('<div class="slide"></div>');
     const figOne = $('<figure></figure>');
-    const img1 = $('<img src="img/icons/icon-people.png"/>');
+    const img1 = $('<img/>');
     const figCap1 = $('<figcaption></figcaption>');
-    const fig1H2 = $('<h2>Paga a tus amigos</h2>');
-    const fig1P = $('<p class="text-grey">Paga a quien quieras desde donde <br>quieras, sin usar efectivo</p>');
-    figCap1.append(fig1H2);
-    figCap1.append(fig1P);
-    figOne.append(img1);
-    figOne.append(figCap1);
-
     const figTwo = $('<figure></figure>');
-    const img2 = $('<img src="img/icons/happy-person.png"/>');
+    const img2 = $('<img/>');
     const figCap2 = $('<figcaption></figcaption>');
-    const fig2H2 = $('<h2>Sin número de cuenta</h2>');
-    const fig2P = $('<p class="text-grey">Elige a quien pagar desde tu lista <br>de contactos</p>');
-    // figTwo.append(img2);
-    // figTwo.append(figCap2);
-
     const figThree = $('<figure></figure>');
-    const img3 = $('<img src="img/icons/happy-person.png"/>');
+    const img3 = $('<img/>');
     const figCap3 = $('<figcaption></figcaption>');
-    const fig3H2 = $('<h2>Gratis y Seguro</h2>');
-    const fig3P = $('<p class="text-grey">La transferencia es inmediata y <br>gratuita de una cuenta a otra</p>');
-
-    divSlide.append(figOne);
-
-    const button = $('<button type="button" class="btn btn-register">Registrarme</button>');
-
-    button.on('click', (e) => {
-       e.preventDefault();
-       state.page = 1;
-       update();
-    });
+    const button = $('<button type="button" class="button">Registrarme</button>');
 
     section.append(divSlide);
     section.append(button);
-    return section;
+    return section;*/
 }
