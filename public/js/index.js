48dd20e9cb9c5246f56433d7f8486032e82ec6c0
Luz Gutierrez <mili01gm@gmail.com>
2017-06-22 09:34:04 -0500

    pide commitear

--- public/js/index.js
+++ public/js/index.js
@@ -5,16 +5,17 @@ const render = (root) => {
 
     const wrapper = $('<div class="wrapper"></div>');
     if (state.page == null) {
+        wrapper.append(Home(_ => render(root)));
+    } else if (state.page == 1) {
+        wrapper.append(RegOne(_ => render(root)));
+    } else if (state.page == 2) {
+        wrapper.append(Code(_ => render(root)));
+    } else if (state.page == 3) {
+        wrapper.append(Code(_ => render(root)));
+    } else if (state.page == 4) {
         wrapper.append(RegUser(_ => render(root)));
     }
-    //     wrapper.append(Home(_ => render(root)));
-    // } else if (state.page == 1) {
-    //     wrapper.append(RegOne(_ => render(root)));
-    // } else if (state.page == 2) {
-    //     wrapper.append(Code(_ => render(root)));
-    // } else if (state.page == 3) {
-    //     wrapper.append(Code(_ => render(root)));
-    // }
+
 
     root.append(wrapper);
 
