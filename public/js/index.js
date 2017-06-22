07865c64f12a7799d4e505e38eac17096bd785fb
Luz Gutierrez <mili01gm@gmail.com>
2017-06-21 22:21:44 -0500

    Descomentando código en index.js

--- public/js/index.js
+++ public/js/index.js
@@ -5,16 +5,16 @@ const render = (root) => {
 
     const wrapper = $('<div class="wrapper"></div>');
     if (state.page == null) {
+        wrapper.append(Home(_ => render(root)));
+    } else if (state.page == 1) {
+        wrapper.append(RegOne(_ => render(root)));
+    } else if (state.page == 2) {
+        wrapper.append(Code(_ => render(root)));
+    } else if (state.page == 3) {
+        wrapper.append(Code(_ => render(root)));
+    } else if (state.page == 4) {
         wrapper.append(RegUser(_ => render(root)));
     }
-    //     wrapper.append(Home(_ => render(root)));
-    // } else if (state.page == 1) {
-    //     wrapper.append(RegOne(_ => render(root)));
-    // } else if (state.page == 2) {
-    //     wrapper.append(Code(_ => render(root)));
-    // } else if (state.page == 3) {
-    //     wrapper.append(Code(_ => render(root)));
-    // }
 
     root.append(wrapper);
 
@@ -22,8 +22,10 @@ const render = (root) => {
 
 const state = {
     user: null,
+
     page: null
 }
+s
 
 $(_ => {
     const root = $('.root');
